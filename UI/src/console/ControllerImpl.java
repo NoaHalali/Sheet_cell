b5635f10acc7394ad1e23824de7d45b98ac50d15
NoaHalali <noa4000@gmail.com>
2024-08-17 12:10:14 +0300

    some changes in UI

--- UI/src/console/ControllerImpl.java
+++ UI/src/console/ControllerImpl.java
@@ -8,30 +8,31 @@ public class ControllerImpl implements Controller {
     private EngineImpl engine = new EngineImpl();
     private InputHandler inputHandler = new InputHandler();
     private OutputHandler outputHandler = new OutputHandler();
+    private Menu menu = new Menu();
     boolean systemIsRunning = true;
 
     @Override
     public void runSystem()
     {
         while (systemIsRunning) {
-            outputHandler.printMenu();
+            menu.printMenu();
             //
             //
 
 
         }
     }
-
-    public void displayCellData(String cellId) {
-        //TODO - validation checking
-
-        CellDTO cellData = engine.getCellData(cellId);
-        System.out.println("Cell ID: " + cellId);
-        System.out.println("Original Value: " + cellData.getOriginalValue());
-        System.out.println("Effective Value: " + cellData.getEffectiveValue());
-        System.out.println("Version: " + cellData.getVersionNumber());
-        System.out.println("Dependencies: " + String.join(", ", cellData.getDependencies()));
-    }
+//
+//    public void displayCellData(String cellId) {
+//        //TODO - validation checking
+//
+//        CellDTO cellData = engine.getCellData(cellId);
+//        System.out.println("Cell ID: " + cellId);
+//        System.out.println("Original Value: " + cellData.getOriginalValue());
+//        System.out.println("Effective Value: " + cellData.getEffectiveValue());
+//        System.out.println("Version: " + cellData.getVersionNumber());
+//        System.out.println("Dependencies: " + String.join(", ", cellData.getDependencies()));
+//    }
 
 
 
--- UI/src/console/Menu.java
+++ UI/src/console/Menu.java
@@ -1,4 +1,9 @@
 package console;
 
 public class Menu {
+    public void printMenu() {
+        System.out.println("Choose a number of action to perform:");
+        ///
+        ///
+    }
 }
