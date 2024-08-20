d1ed8cb56a87097d4373074c419c4545d8cf163c
NoaHalali <noa4000@gmail.com>
2024-08-20 18:39:57 +0300

    Continued with FileManager functions

--- engine/src/XMLFile/FileManager.java
+++ engine/src/XMLFile/FileManager.java
@@ -1,5 +1,6 @@
 package XMLFile;
 
+import XMLFile.GeneratedFiles.STLCell;
 import XMLFile.GeneratedFiles.STLSheet;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
@@ -12,17 +13,27 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class FileManager {
-    private final static String JAXB_XML_GAME_PACKAGE_NAME = "XMLFile/GeneratedFiles";
+
+    private static final String JAXB_XML_GAME_PACKAGE_NAME = "XMLFile/GeneratedFiles";
+    private final int maxRows = 50;
+    private final int minRows = 1;
+    private final int maxCols = 20;
+    private final int minCols = 1;
+
     //validate path
     //load from xml to stlsheet
     //stl sheet check
     //stlsheet to sheet
 
-    public Sheet processFile(String filePath) throws FileNotFoundException,IllegalArgumentException {
+    public Sheet processFile(String filePath) throws FileNotFoundException, IllegalArgumentException {
 
         validatePath(filePath);
-        STLSheet tmpSheet =loadXML(filePath);
+        STLSheet XMLSheet = loadXML(filePath);
+        validateSheetSize(XMLSheet);
+        vaidateCells(XMLSheet);
+        Sheet sheet = convertSLTSheetToSheet(XMLSheet);
 
+        return sheet;
     }
 
 
@@ -38,18 +49,13 @@ public class FileManager {
         }
     }
 
-    public STLSheet loadXML(String filePath)
-    {
-        STLSheet sheet=null;
+    public STLSheet loadXML(String filePath) {
         try {
             InputStream inputStream = new FileInputStream(new File(filePath));
-
-            sheet = deserializeFrom(inputStream);
-
+            return deserializeFrom(inputStream);
         } catch (JAXBException | FileNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Failed to load XML file", e);
         }
-        return sheet;
     }
 
     private STLSheet deserializeFrom(InputStream in) throws JAXBException {
@@ -59,13 +65,64 @@ public class FileManager {
         return (STLSheet) unmarshaller.unmarshal(in);
     }
 
-    public void validateXmlSheet(STLSheet sheet) {
+    public void validateSheetSize(STLSheet sheet) throws IllegalArgumentException {
+        int rows = sheet.getSTLLayout().getRows();
+        int cols = sheet.getSTLLayout().getColumns();
 
+        if (rows < minRows || rows > maxRows) {
+            throw new IllegalArgumentException(String.format("Rows number must be between %d and %d.", minRows, maxRows));
+        }
+        if (cols < minCols || cols > maxCols) {
+            throw new IllegalArgumentException(String.format("Cols number must be between %d and %d.", minRows, maxRows));
+        }
+    }
+
+    private void vaidateCells(STLSheet xmlSheet) {
+        checkCellInBoardRange(xmlSheet);
+        //TODO - 4
+    }
+
+    public void checkCellInBoardRange(STLSheet sheet) throws IllegalArgumentException{
+
+        for(STLCell cell : sheet.getSTLCells().getSTLCell()){
+            String colStr = cell.getColumn();
+            if(colStr.length() != 1){
+                throw new IllegalArgumentException("Column must be between 'A' to 'T'.");
+            }
+
+            int cellCol = colStr.charAt(0)-'A';
+            int givenRowsRange = sheet.getSTLLayout().getRows();
+            int givenColsRange = sheet.getSTLLayout().getColumns();
+
+            if(cell.getRow() < minRows||cell.getRow()> givenRowsRange ){
+                throw new IllegalArgumentException(String.format("Cell row must be between %d and %d.", minRows, givenRowsRange));
+            }
+            else if(cellCol < minCols || cellCol > givenColsRange){
+                throw new IllegalArgumentException(String.format("Cell column must be between %d and %d.", minCols, givenColsRange));
+            }
+        }
     }
 
 
 
+
 }
+//        public class ValueOutOfRangeException extends RuntimeException {
+//            public ValueOutOfRangeException(String message) {
+//                super(message);
+//            }
+//        }
+        //למשל
+//        public void setPercentage(int percentage) {
+//            if (percentage < 0 || percentage > 100) {
+//                throw new ValueOutOfRangeException("Percentage must be between 0 and 100.");
+//            }
+//            // Set the percentage
+//        }
+
+
+
+
 
 
 
